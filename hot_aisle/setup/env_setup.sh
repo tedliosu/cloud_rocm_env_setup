06dc9d0c3b3c0c7aed31bc18d23014472935412d
Ted Li <li.6998@osu.edu>
2026-04-05 04:45:51 -0400

    attempt fix dkms detection logic

--- hot_aisle/setup/env_setup.sh
+++ hot_aisle/setup/env_setup.sh
@@ -14,7 +14,7 @@ if ! grep --quiet "DISTRIB_ID=${EXPECTED_DISTRO}" /etc/lsb-release ||
     echo -e "\n    This IS NOT ${EXPECTED_DISTRO} ${EXPECTED_DIST_VER}; bailing!" >&2
     exit 1
 fi
-if ! dkms status amdgpu | grep --quiet installed; then
+if ! dkms status amdgpu | grep --quiet "installed"; then
     echo "Out of tree amdgpu dkms not installed; bailing!" >&2
     exit 1
 fi
