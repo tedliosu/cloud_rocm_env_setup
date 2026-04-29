2c34fc472c4231a99b033c053540162aa34890a6
Ted Li <li.6998@osu.edu>
2026-04-28 21:48:02 -0400

    handle unset var 1

--- validate/lib/vald_util_funcs.sh
+++ validate/lib/vald_util_funcs.sh
@@ -39,6 +39,10 @@ validate_basic_hipco() {
         echo "FAILED to detect 'ROCM_HOME_DIR'!" >&2
         exit 1
     }
+    # IMPORTANT - env var could be unset, so we check!
+    if [ -z "${CMAKE_PREFIX_PATH+hasval}" ]; then
+        CMAKE_PREFIX_PATH=""
+    fi
     env CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${ROCM_HOME_DIR}/lib/cmake" \
         cmake -DCMAKE_HIP_ARCHITECTURES="$3" -S "$1" -B "$1/${_build_dir_name}"
     cmake --build "$1/${_build_dir_name}" --target "${_example_bin_name}"
