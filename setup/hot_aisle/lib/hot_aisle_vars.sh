3cfc3d060a4bf002d3149c0b224f11d4e9b3a599
Ted Li <li.6998@osu.edu>
2026-08-08 21:23:33 -0400

    Hot Aisle env target assumptions update

--- setup/hot_aisle/lib/hot_aisle_vars.sh
+++ setup/hot_aisle/lib/hot_aisle_vars.sh
@@ -1,7 +1,7 @@
 
 export EXPECTED_DISTRO="Ubuntu"
-export EXPECTED_DIST_VER="22.04"
-export EXPECTED_DIST_CODENAME="jammy"
+export EXPECTED_DIST_VER="24.04"
+export EXPECTED_DIST_CODENAME="noble"
 export EXPECTED_ROCM_VER="7.2"
 EXPECTED_ROCMVER_REGEX="$(printf "%s" "^${EXPECTED_ROCM_VER}.[0-9]+" | sed 's/\./\\./g')"
 export EXPECTED_ROCMVER_REGEX
